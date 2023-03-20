3d4d6f96d25f8f60a750e908c625c641914b2c89
Kian Zareinajad <kzareinajad@gmail.com>
2023-03-20 18:37:44 +0000

    save and delete js

--- js/main.js
+++ js/main.js
@@ -16,17 +16,32 @@ const notes = [
 
 class NotesAPI {
     static getAllNotes() {
-        const savedNotes = notes || [];
+        const savedNotes = JSON.parse(localStorage.getItem("notes-app")) || [];
         return savedNotes.sort((a, b) => {
             return new Date(a.updated) > new Date(b.updated) ? -1 : 1
         });
     }
-    saveNote() {
+    static saveNote(noteToSave) {
+        // 1. existed or Not
+        const notes = NotesAPI.getAllNotes();
+        const existedNote = notes.find((n) => n.id == noteToSave.id);
 
+        if (existedNote) {
+            existedNote.title = noteToSave.title;
+            existedNote.body = noteToSave.body;
+            existedNote.updated = new Date().toISOString();
+        } else {
+            noteToSave.id = new Date().getTime();
+            noteToSave.updated = new DataTransfer().toISOString();
+            notes.push(noteToSave)        
+        }
+        localStorage.setItem("notes-app", JSON.stringify(notes));
     }
-    deleteNote() {
-
+    static deleteNote(id) {
+        const notes = NotesAPI.getAllNotes();
+        const filtedrNotes = notes.filter(n => n.id !== id);
+        localStorage.setItem("notes-app", JSON.stringify(filtedrNotes));
     }
 };
 
-console.log(NotesAPI.getAllNotes())
+console.log(NotesAPI.deleteNote());
